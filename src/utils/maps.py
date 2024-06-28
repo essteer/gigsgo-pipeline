06f9ac4af90d6bb7cde84dde5331aa9cb5873107
essteer <essteer@pm.me>
2024-06-28 20:05:42 +0800

    feat: Move content mappings to maps.py

---
+++ src/utils/maps.py
@@ -0,0 +1,29 @@
+DAY_MAP = {
+    "monday": 1,
+    "tuesday": 2,
+    "wednesday": 3,
+    "thursday": 4,
+    "friday": 5,
+    "saturday": 6,
+    "sunday": 7,
+}
+
+TIER_MAP = {
+    "1-day ticket 一日票": "1_day",
+    "3-day ticket 三日票": "3_day",
+    "advance 預售": "advance",
+    "after party": "after_party",
+    "door 即場": "door",
+    "early-bird 早鳥": "advance",
+    "fringe club member 藝穗會會員": "member",
+    "full time student 全日制學生": "student",
+    "monthly pass 月票": "monthly_pass",
+    "regular 正價": "standard",
+    "student 學生": "student",
+    "vip": "vip",
+    "walk-in 即場": "door",
+}
+
+GENRE_MAP = {"r'n'b": "r&b", "rnb": "r&b", "r&b": "r&b"}
+
+LOCATION_LIST = ["Japan 日本"]
